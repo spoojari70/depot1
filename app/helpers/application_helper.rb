c69b21c83f559676ca4bb6817eb3316b36fee0f4
Sairaj <sairajpoojari1@gmail.com>
2023-10-03 17:46:37 +0530

    THIRD COMMIT

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,9 @@
 module ApplicationHelper
+  def render_if(condition, record)
+    if condition
+      render record
+    end
+  end
   def total_cart_item
     if session[:cart_id].present?
       @cart = Cart.find(session[:cart_id])
--- test/controllers/line_items_controller_test.rb
+++ test/controllers/line_items_controller_test.rb
@@ -12,3 +12,13 @@ test "should update line_item" do
   params: {line_item: { product_id: @line_item.product_id } }
   assert_redirected_to line_item_url(@line_item)
 end
+
+
+test "should create line_item via ajax" do
+  assert_difference('LineItem.count') do
+    post line_items_url, params: { product_id: products(:ruby).id },
+    xhr: true
+  end
+  assert_response :success
+  assert_match /<tr class=\\"line-item-highlight/, @response.body
+end
